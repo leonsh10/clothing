e06d6b77bfddc5ac2b6ecc49ad13dd1001958e1c
elantra jashari <elantrajashari@outlook.com>
2022-02-04 01:14:49 +0100

    Contact - page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <Header />
-    <h1>This is an about page</h1>
+    
     <div class="modal fade bg-white" id="templatemo_search" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg" role="document">
             <div class="w-100 pt-1 mb-5 text-right">
