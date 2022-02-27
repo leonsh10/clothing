6b852d75dd3b5c2183dfd20e5edb79b6fda83cc0
Leon Shala <ls48316@ubt-uni.net>
2022-02-27 23:33:26 +0100

    changes in contact

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -2,25 +2,7 @@
   <div>
     <Header />
 
-    <div class="container-fluid bg-light py-5">
-      <div class="col-md-6 m-auto text-center">
-        <h1 class="h1" style="font-family: Times New Roman">Contact Us</h1>
-        <p>
-          Proident, sunt in culpa qui officia deserunt mollit anim id est
-          laborum. Lorem ipsum dolor sit amet.
-        </p>
-      </div>
-    </div>
-    <div class="map">
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2935.343786761927!2d21.149849415806784!3d42.63287127916947!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13549eed611519f3%3A0x67f0cab2bd31c914!2sAlbi%20Mall!5e0!3m2!1sen!2s!4v1571741329054!5m2!1sen!2s"
-        width="100%"
-        height="380"
-        frameborder="0"
-        style="border: 0"
-        allowfullscreen=""
-      ></iframe>
-    </div>
+   
 
     <div class="container py-5">
       <div class="row py-5">
@@ -122,7 +104,16 @@
         </form>
       </div>
     </div>
-
+ <div class="map">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2935.343786761927!2d21.149849415806784!3d42.63287127916947!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13549eed611519f3%3A0x67f0cab2bd31c914!2sAlbi%20Mall!5e0!3m2!1sen!2s!4v1571741329054!5m2!1sen!2s"
+        width="100%"
+        height="380"
+        frameborder="0"
+        style="border: 0"
+        allowfullscreen=""
+      ></iframe>
+    </div>
     <Footer />
   </div>
 </template>
